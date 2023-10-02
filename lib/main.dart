e93830b47500c65db55c19708a59eb235682f3a0
Howard Tai <howard@howardism.dev>
2023-10-02 23:42:04 +0800

    chore: update folder structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,5 @@
-import 'package:firstapp/meal_app/screens/tabs.dart';
+import 'package:firstapp/meal_app/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:google_fonts/google_fonts.dart';
 
-void main() => runApp(const ProviderScope(child: App()));
-
-final theme = ThemeData(
-  useMaterial3: true,
-  colorScheme: ColorScheme.fromSeed(
-    brightness: Brightness.dark,
-    seedColor: const Color.fromARGB(255, 131, 57, 0),
-  ),
-  textTheme: GoogleFonts.latoTextTheme(),
-);
-
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: theme,
-      home: const TabsScreen(),
-    );
-  }
-}
+void main() => runApp(const ProviderScope(child: MealApp()));
---
+++ lib/meal_app/main.dart
@@ -0,0 +1,24 @@
+import 'package:firstapp/meal_app/screens/tabs.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final theme = ThemeData(
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(
+    brightness: Brightness.dark,
+    seedColor: const Color.fromARGB(255, 131, 57, 0),
+  ),
+  textTheme: GoogleFonts.latoTextTheme(),
+);
+
+class MealApp extends StatelessWidget {
+  const MealApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: theme,
+      home: const TabsScreen(),
+    );
+  }
+}
